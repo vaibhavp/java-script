8681f9d9c7c0e11529d4446760220513b9763b1f
vp15591 <vp15591@gmail.com>
2017-04-04 00:12:44 +0530

    new changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ //$("#main").append("Vaibhav Pathak");
+
+
+
+var formatted_name = HTMLheaderName.replace("%data%","Vaibhav Pathak");
+$("#header").append(formatted_name);
+
+var formatted_role = HTMLheaderRole.replace("%data%","DevOps Engineer");
+$("#header").append(formatted_role);
